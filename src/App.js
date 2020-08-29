13b42bc26cb5426734152274dc4bb0433c3b0564
jshou <joshuahou@gmail.com>
2020-08-28 20:56:20 -0700

    skeleton hero blade

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          UX &middot; Interaction &middot; Visual &middot; Illustration
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <img src="/img/hou_logo.png" alt="HOU"/>
+        <button>VIEW WORK</button>
       </header>
     </div>
   );
