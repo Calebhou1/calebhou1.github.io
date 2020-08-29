d707be22d6610c9f4eb6d1bc67c426b086ae332e
jshou <joshuahou@gmail.com>
2020-08-29 09:59:40 -0700

    wip

--- src/Disney.jsx
+++ src/Disney.jsx
@@ -9,6 +9,28 @@ const Disney = () => (
     // hero img
   >
     blah de blah
+    <h3>SUMMARY</h3>
+    <p>
+      A website redesign for Walt Disney World. The current web experience is a
+      smorgasbord of information that is aimed at no one in particular. This
+      project was aimed at providing more personalization and contextual
+      information for a guest before, during, and after their visit.
+    </p>
+    <p>
+      Research and testing was performed across 10,000 individuals to prove the
+      problem case and provide validation.
+    </p>
+    <p>For more information, please get in touch.</p>
+    <dl>
+      <dt>MY ROLE</dt>
+      <dd>UX design, Visual design</dd>
+
+      <dt>DURATION</dt>
+      <dd>8 weeks</dd>
+
+      <dt>TEAM</dt>
+      <dd>3 Design, 4 Dev, 2 PM</dd>
+    </dl>
   </CaseStudy>
 );
 
--- src/components/CaseStudy.jsx
+++ src/components/CaseStudy.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import "../css/CaseStudy.scss";
 
-const CaseStudy = ({ title, subtitle, children }) => (
-  <div className="CaseStudy">
-    <h1>{title}</h1>
-    <h2>{subtitle}</h2>
-    {children}
-  </div>
-);
+const CaseStudy = ({ title, subtitle, children }) => {
+  let history = useHistory();
+  return (
+    <div className="CaseStudy">
+      <div className="CaseStudy__hero">
+        <h1>{title}</h1>
+        <h2>{subtitle}</h2>
+        <button onClick={history.goBack}>BACK</button>
+      </div>
+
+      {children}
+    </div>
+  );
+};
 
 export default CaseStudy;
