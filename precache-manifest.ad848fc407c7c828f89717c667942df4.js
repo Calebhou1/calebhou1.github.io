5fcfdbfacaf4e101e477cd053a9f67c0702499e9
jshou <joshuahou@gmail.com>
2020-08-30 09:21:50 -0700

    Updates

---
+++ precache-manifest.ad848fc407c7c828f89717c667942df4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "082ffa639202b865b8a9fd00784a0f62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4da1f93ffefd5a8a1a81",
+    "url": "/static/css/main.811a5a4f.chunk.css"
+  },
+  {
+    "revision": "f7e7b66fdb930d0b5bb4",
+    "url": "/static/js/2.45e3974e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.45e3974e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4da1f93ffefd5a8a1a81",
+    "url": "/static/js/main.5bd46117.chunk.js"
+  },
+  {
+    "revision": "54a79506b718565c71de",
+    "url": "/static/js/runtime-main.ffc890be.js"
+  },
+  {
+    "revision": "f9578d7d7cb25868d19221ef7fa14941",
+    "url": "/static/media/caleb_profile.f9578d7d.png"
+  },
+  {
+    "revision": "8a546e1304c6b011c5d22f818866f5a6",
+    "url": "/static/media/hou_logo.8a546e13.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/calebhou1.github.io/precache-manifest.0006435fa9893285569edf7a46a00f71.js"
+  "/precache-manifest.ad848fc407c7c828f89717c667942df4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/calebhou1.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
