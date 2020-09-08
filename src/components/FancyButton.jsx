65870fd2a5d489f0e7ebd9c5d6434edba695852c
Josh Hou <josh@carbonfive.com>
2020-09-08 12:57:13 -0700

    fix button height

--- src/components/FancyButton.jsx
+++ src/components/FancyButton.jsx
@@ -9,12 +9,12 @@ export default ({ className, containerClass, onClick, svgClass, children }) => (
       <button className={cx("btn", "button", className)} onClick={onClick}>
         <svg
           width="180px"
-          height="60px"
-          viewBox="0 0 180 60"
+          height="48px"
+          viewBox="0 0 180 48"
           className={cx("border", svgClass)}
         >
-          <polyline points="179,1 179,59 1,59 1,1 179,1" className="bg-line" />
-          <polyline points="179,1 179,59 1,59 1,1 179,1" className="hl-line" />
+          <polyline points="179,1 179,47 1,47 1,1 179,1" className="bg-line" />
+          <polyline points="179,1 179,47 1,47 1,1 179,1" className="hl-line" />
         </svg>
         <span>{children}</span>
       </button>
--- src/components/FancyLink.jsx
+++ src/components/FancyLink.jsx
@@ -22,12 +22,12 @@ export default ({
       >
         <svg
           width="180px"
-          height="60px"
-          viewBox="0 0 180 60"
+          height="48px"
+          viewBox="0 0 180 48"
           className="border"
         >
-          <polyline points="179,1 179,59 1,59 1,1 179,1" className="bg-line" />
-          <polyline points="179,1 179,59 1,59 1,1 179,1" className="hl-line" />
+          <polyline points="179,1 179,47 1,47 1,1 179,1" className="bg-line" />
+          <polyline points="179,1 179,47 1,47 1,1 179,1" className="hl-line" />
         </svg>
         <span>{children}</span>
       </a>
