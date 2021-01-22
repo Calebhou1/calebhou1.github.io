d0f5c9e3cc6681df0dd4482238e1bc69c758128d
Josh Hou <josh@carbonfive.com>
2021-01-22 08:45:30 -0800

    alts

--- src/components/BungieImage.jsx
+++ src/components/BungieImage.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default ({ src, caption }) => (
   <>
-    <a href={src} target="_blank" rel="noopener">
-      <img src={src} />
+    <a href={src} target="_blank" rel="noopener noreferrer">
+      <img src={src} name={caption} alt={caption} />
     </a>
     <p>{caption}</p>
   </>
